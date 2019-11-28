bb5fce79d732679593eb4a5446598dc3043b585c
Giovanni <gpetruc@gmail.com>
2019-11-28 13:16:27 +0100

    array_partition the input

--- tutorial/part1/src/func.cc
+++ tutorial/part1/src/func.cc
@@ -2,6 +2,9 @@
 
 
 ap_int<30> myfunc(const ap_int<16> a[NDATA], const ap_int<16> b[NDATA], ap_int<24> c[NDATA]) {
+    #pragma HLS array_partition variable=a complete
+    #pragma HLS array_partition variable=b complete
+    #pragma HLS array_partition variable=c complete
     #pragma HLS pipeline II=1
 
     int sum = 0;
