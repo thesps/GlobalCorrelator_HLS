6faf88cbd680510dbfb16f3d007c7cc137b3274e
Giovanni <gpetruc@gmail.com>
2019-11-28 11:35:02 +0100

    Add #pragma HLS pipeline II=1 (now latency 8, II 6)

--- tutorial/part1/src/func.cc
+++ tutorial/part1/src/func.cc
@@ -2,6 +2,8 @@
 
 
 ap_int<30> myfunc(const ap_int<16> a[NDATA], const ap_int<16> b[NDATA], ap_int<24> c[NDATA]) {
+    #pragma HLS pipeline II=1
+
     int sum = 0;
     for (unsigned int i = 0; i < NDATA; ++i) {
         int prod = (a[i] * b[i]) >> 8;
